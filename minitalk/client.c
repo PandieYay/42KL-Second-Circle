f674041613c860bc58289187eff77e24c9f31036
Edison Lim <babyedison@gmail.com>
2022-04-26 14:43:48 +0800

    Minitalk + Bonus done

--- minitalk/client.c
+++ minitalk/client.c
@@ -1,11 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edlim <edlim@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/26 14:40:39 by edlim             #+#    #+#             */
+/*   Updated: 2022/04/26 14:40:40 by edlim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
+
+static void	handler(int sig, siginfo_t *info, void *useless)
+{
+	(void)sig;
+	(void)useless;
+	ft_putstr("Message has been sent at server: ");
+	ft_putnbr(info->si_pid);
+	write(1, "\n", 1);
+}
+
+static void	sendbits(int pid, char c)
+{
+	int	i;
+
+	i = 0;
+	while (i < 8)
+	{
+		if (c % 2 == 0)
+		{
+			kill(pid, SIGUSR1);
+			usleep(100);
+		}
+		else
+		{
+			kill(pid, SIGUSR2);
+			usleep(100);
+		}
+		c = c >> 1;
+		i++;
+	}
+}
+
+static void	sendstr(int pid, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != 0)
+	{
+		sendbits(pid, str[i]);
+		i++;
+	}
+	sendbits(pid, '\0');
+}
 
 int	main(int argc, char **argv)
 {
-	int	pid;
+	int					pid;
+	struct sigaction	sa;
 
+	sa.sa_sigaction = handler;
+	sigaction(SIGUSR1, &sa, NULL);
 	if (argc != 3)
 		return (-1);
 	pid = ft_atoi(argv[1]);
-	kill(pid, SIGUSR1);
+	sendstr(pid, argv[2]);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -1,21 +1,58 @@
-#include "minitalk.h"
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edlim <edlim@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/26 14:40:42 by edlim             #+#    #+#             */
+/*   Updated: 2022/04/26 14:40:43 by edlim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void handler(int sig)
+#include "minitalk.h"
+
+static void	handler(int sig, siginfo_t *info, void *useless)
 {
-    ft_putstr("I wont die!\n");
-	ft_putnbr(sig);
+	static int	i;
+	static int	character;
+	int			bit;
+
+	bit = 0;
+	(void)useless;
+	if (sig == SIGUSR1)
+		bit = 0;
+	else
+		bit = 1;
+	character += bit << i;
+	i++;
+	if (i == 8)
+	{
+		if (character == '\0')
+		{
+			write(1, "\n", 1);
+			kill(info->si_pid, SIGUSR1);
+		}
+		else
+			write(1, &character, 1);
+		i = 0;
+		character = 0;
+	}
 }
 
 int	main(void)
 {
-	int	pid;
-	struct sigaction sa;
-	sa.sa_handler = handler;
+	int					pid;
+	struct sigaction	sa;
 
+	sa.sa_sigaction = handler;
+	sa.sa_flags = SA_RESTART;
 	pid = getpid();
 	ft_putstr("Server PID: ");
 	ft_putnbr(pid);
+	write(1, "\n", 1);
 	sigaction(SIGUSR1, &sa, NULL);
-	while(1);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
 }
